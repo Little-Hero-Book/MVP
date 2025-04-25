c3f7582ad473fd17186675eab7667dacfc8052b7
Wayne Leung <waynetwleung@gmail.com>
2025-04-24 19:04:45 -0700

    converted server files to ts

---
+++ server/utils/supabase.ts
@@ -0,0 +1,12 @@
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_SERVICE_KEY) {
+  throw new Error('Missing Supabase environment variables');
+}
+
+const supabase: SupabaseClient = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_KEY
+);
+
+export default supabase; 
