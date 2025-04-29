89a431cbc1ebe427493858fd102ed1a32916d63b
Sergey01 <Sergey01@users.noreply.github.com>
2025-04-28 23:51:47 -0400

    wip2 - the progress continues

--- server/controllers/imgGenController.ts
+++ server/controllers/imgGenController.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import OpenAI, { toFile } from 'openai';
 import { readFile, writeFile } from 'fs/promises';
-import { Uploadable } from 'openai/uploads';
+import { FileLike } from 'openai/uploads';
 
 dotenv.config();
 
@@ -9,35 +9,47 @@ const client = new OpenAI();
 const imgModel = 'gpt-image-1';
 const mimeType = 'image/png';
 
-export const readFiles = async (filePaths: string[]): Promise<string[]> => {
-  const readFilePromises = filePaths.map(
-    (path) => readFile(path, { encoding: 'base64' })
-    // readFile(path)
+// export const readFiles = async (filePaths: string[]): Promise<string[]> => {
+//   const readFilePromises = filePaths.map(
+//     (path) => readFile(path, { encoding: 'base64' })
+//     // readFile(path)
+//   );
+//   const base64Files = await Promise.all(readFilePromises);
+//   return base64Files;
+// };
+
+export const readFilesFromDisk = async (
+  filePaths: string[]
+): Promise<FileLike[]> => {
+  const files = await Promise.all(
+    filePaths.map(
+      async (path) => await toFile(readFile(path), null, { type: mimeType })
+    )
   );
-  const base64Files = await Promise.all(readFilePromises);
-  return base64Files;
+  console.log(files[0].constructor.name);
+  return files;
 };
 
 // takes in a prompt string and an array of file paths for context images
 export const generateImage = async (
   prompt: string,
-  contextImgs: string[]
-): Promise<string | undefined> => {
-  const images = await Promise.all(
-    contextImgs.map(
-      async (s) =>
-        await toFile(Buffer.from(s), null, {
-          type: mimeType,
-        })
-    )
-  );
+  contextImgs: FileLike[]
+): Promise<string> => {
+  // const images = await Promise.all(
+  //   contextImgs.map(
+  //     async (s) =>
+  //       await toFile(Buffer.from(s), null, {
+  //         type: mimeType,
+  //       })
+  //   )
+  // );
   const response = await client.images.edit({
     model: imgModel,
-    image: images,
+    image: contextImgs,
     prompt: prompt,
   });
   const image_base64 = response.data ? response.data[0].b64_json : '';
-  return image_base64;
+  return image_base64 ? image_base64 : '';
 };
 
 const main = async () => {
@@ -45,9 +57,10 @@ const main = async () => {
   const testPrompt =
     "Combine the two images into a children's coloring book format. Cartoonify the human in the photo and then have him sit at a table with everyone, eating.";
   const testFiles = ['input.png', 'input2.png'];
-  const files = await readFiles(testFiles);
+  const files = await readFilesFromDisk(testFiles);
   const output = await generateImage(testPrompt, files);
-  await writeFile(outputFile, output, 'utf8');
+  await writeFile(outputFile, output, 'base64');
 };
 
 main();
+// readFilesFromDisk(['input.png', 'input2.png']);
